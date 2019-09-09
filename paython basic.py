f3f887344afca5d6cad5a8453a9ec74f6ef42663
Lucka <luckabarbeau@gmail.com>
2019-09-09 15:53:08 -0400

    petit code retour sur les base python

---
+++ paython basic.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+import scipy as sci
+import matplotlib as matplot
+
+
+for i in range(0,10):
+    print(i)
+    
+
+
