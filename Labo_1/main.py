7a11c54692a3b2a6316be872b482e55a4df91a26
Matthew Coffey <mattcof@gmail.com>
2019-09-09 17:48:23 -0400

    added main

---
+++ Labo_1/main.py
@@ -0,0 +1,9 @@
+# Read all files and add data to appropriate arrays
+import read_file
+
+mesh4_2 = read_file.read_file("p242_T_directe_4x2.txt")
+mesh8_4 = read_file.read_file("p242_T_directe_8x4.txt")
+mesh16_8 = read_file.read_file("p242_T_directe_16x8.txt")
+mesh32_16 = read_file.read_file("p242_T_directe_32x16.txt")
+mesh64_32 = read_file.read_file("p242_T_directe_64x32.txt")
+mesh128_64 = read_file.read_file("p242_T_directe_128x64.txt")
