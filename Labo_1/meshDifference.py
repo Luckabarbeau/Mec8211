09ab466c8d7099b723648c81ece656157335bb89
Matthew Coffey <mattcof@gmail.com>
2019-09-09 17:34:00 -0400

    first commit of meshDifference function

---
+++ Labo_1/meshDifference.py
@@ -0,0 +1,5 @@
+# This function calculates the difference between two meshes.
+# The difference is calculated by subtracting the the value of the coarser mesh
+# from the sum of the values of the finer mesh covered by each element of the coarser
+# mesh.
+
