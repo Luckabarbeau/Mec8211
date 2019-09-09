448b1575be4813b8bb4947b91d2a3ea3f355536a
Lucka <luckabarbeau@gmail.com>
2019-09-09 16:28:22 -0400

    start labo 1 whit data

---
+++ python basic.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+import scipy as sci
+import matplotlib as matplot
+
+
+for i in range(0,10):
+    print(i)
+    
+class mymath:
+    def product(self,X,Y):
+        return X*Y
+    def square(self,X):
+        return X**2
+    def special(self,X,Y):
+        return X**Y
+    
+    self.X=1
+    self.Y=2
+    
+print(mymath.product())
+
