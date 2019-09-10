6f378b0cff9065fed30d3fe1469845b289c0b3c4
Lucka <luckabarbeau@gmail.com>
2019-09-10 09:00:03 -0400

    start the work on mesh error

--- Labo_1/meshDifference.py
+++ Labo_1/meshDifference.py
@@ -2,4 +2,14 @@
 # The difference is calculated by subtracting the the value of the coarser mesh
 # from the sum of the values of the finer mesh covered by each element of the coarser
 # mesh.
+import numpy as np
 
+def mesh_difference(mesh_c,mesh_f):
+    shape_c=mesh_c.shape
+    shape_f=mesh_f.shape
+    
+    for i in range(mesh_f[1]+1):
+        
+    return 
+
+mesh_difference(np.zeros((4,2)),np.ones((8,4)))
