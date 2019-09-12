aee9b142d9882655d9c20e71a4c51645aa28c476
Lucka <luckabarbeau@gmail.com>
2019-09-12 14:45:34 -0400

    update mesh main and error function

--- Labo_1/plot_error.py
+++ Labo_1/plot_error.py
@@ -1,11 +1,22 @@
-import matplotlib
+import matplotlib.pyplot as plt
 import numpy as np
 
 
-def graph(error , nb_cell):
+def plot_error(error , nb_cell):
     log_e = np.log(error)
     log_nb = np.log(1/nb_cell)
     a , b = np.polyfit(log_nb , log_e , 1)
 
-    matplotlib.pyplot.loglog(matplotlib)
+    plt.plot(log_nb,log_e,'bo')
+    X_1=np.zeros((2,1))
+    Y_1 = np.zeros((2, 1))
+    X_1[0] = np.min(log_nb)
+    X_1[1] = np.max(log_nb)
+    Y_1[0] = np.min(log_nb)*a+b
+    Y_1[1] = np.max(log_nb)*a+b
+    plt.xlabel('ln(1/nx)')
+    plt.ylabel( 'ln(Error)')
+    plt.title(('log log curve of error in function of the cell size , Slop =',str(a)))
+    plt.plot(log_nb, log_e, 'bo')
+    plt.plot(X_1, Y_1,)
     return a , b
